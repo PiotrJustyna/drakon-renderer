c12c5ffd2478a3363012ff693e1237a6a2c3b7c1
Piotr Justyna <piotr.justyna@gmail.com>
2024-02-27 16:49:12 +0000

    automatic connections between steps

---
+++ HelloWorld.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
+
+data Step = Step {
+    originCoordinates :: Point V2 Double,
+    name :: Double }
+
+stepName :: Step -> Double
+stepName Step { originCoordinates = x, name = y } = y
+
+stepShape :: Double -> Diagram B
+stepShape x = fromOffsets
+    [V2 0 0,
+    V2 0 0.5,
+    V2 0.75 0,
+    V2 0 (-0.5),
+    V2 (-0.75) 0]
+    # showOrigin
+    # named x
+
+steps :: [Step]
+steps = [
+    Step {
+        originCoordinates = p2 (x, y),
+        name = x * 10 + y }
+    | x <- [0 .. 1],
+    y <- [0 .. 2]]
+
+connections :: [Step] -> [QDiagram B V2 Double Any -> QDiagram B V2 Double Any]
+connections (x1: x2: xn) =
+    connectOutside (stepName x1) (stepName x2): connections (x2: xn)
+connections (x1: []) = []
+connections [] = []
+
+main = mainWith $
+    position [(x, (stepShape y)) | Step { originCoordinates = x, name = y } <- steps]
+    # applyAll (connections steps)
+    # lw veryThin
--- README.md
+++ README.md
@@ -12,13 +12,12 @@
 * `cabal install --lib diagrams-lib`
 * `cabal install --lib diagrams-svg`
 * `cabal install --lib base`
-* `ghc HelloWorld.lhs` 
+* `ghc HelloWorld.hs` 
 * `./HelloWorld -o hello-world.svg -w 400`
 
 ![hello-world](./hello-world.svg)
 
 ## resources
 
-* [literate programming](https://wiki.haskell.org/Literate_programming)
 * [drakon](https://drakonhub.com/read/docs)
 * [diagrams](https://archives.haskell.org/projects.haskell.org/diagrams/doc/quickstart.html#introduction)
