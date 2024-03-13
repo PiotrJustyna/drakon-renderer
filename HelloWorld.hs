1736484004a7239b21f5d0c15049383ba3a156b6
Piotr Justyna <piotr.justyna@gmail.com>
2024-03-13 16:32:25 +0000

    basic decision step rendering done

--- HelloWorld.hs
+++ HelloWorld.hs
@@ -9,6 +9,7 @@ import Diagrams.TwoD.Text
 data StepType
     = StartType
     | RegularStep
+    | DecisionStep
     | EndType
 
 data Step = Step {
@@ -23,10 +24,22 @@ stepShape :: Double -> Diagram B
 stepShape x = rect 0.95 0.4 # showOrigin # named x
 
 startShape :: Double -> Diagram B
-startShape x = text "start" # fontSize (local 0.1) # light # font "courier" <> roundedRect 0.95 0.4 0.5 # showOrigin # named x
+startShape x = text "start" # fontSize (local 0.1) # light # font "courier" <> roundedRect 1.0 0.4 0.5 # showOrigin # named x
 
 endShape :: Double -> Diagram B
-endShape x = text "end" # fontSize (local 0.1) # thinWeight # font "courier" <> roundedRect 0.95 0.4 0.5 # showOrigin # named x
+endShape x = text "end" # fontSize (local 0.1) # thinWeight # font "courier" <> roundedRect 1.0 0.4 0.5 # showOrigin # named x
+
+decisionShape :: Double -> Diagram B
+decisionShape x = fromOffsets
+    [V2 (-0.1) 0.2,
+    V2 0.1 0.2,
+    V2 0.8 0.0,
+    V2 0.1 (-0.2),
+    V2 (-0.1) (-0.2),
+    V2 (-0.8) (0.0)]
+    # translate (r2 ((-0.4), (-0.2)))
+    # showOrigin
+    # named x
 
 uniqueName :: Double -> Double -> Double
 uniqueName x y = x * 10 + (abs y)
@@ -34,8 +47,9 @@ uniqueName x y = x * 10 + (abs y)
 steps :: [Step]
 steps =
     Step { originCoordinates = p2 (0, 0), name = uniqueName 0 0, stepType = StartType }
-    : [Step { originCoordinates = p2 (x, y), name = uniqueName x y, stepType = RegularStep } | x <- [0], y <- [-1, -2, -3]]
-    ++ [Step { originCoordinates = p2 (0, -4), name = uniqueName 0 4, stepType = EndType }]
+    : Step { originCoordinates = p2 (0, -1), name = uniqueName 0 (-1), stepType = DecisionStep }
+    : [Step { originCoordinates = p2 (x, y), name = uniqueName x y, stepType = RegularStep } | x <- [0], y <- [-2, -3, -4]]
+    ++ [Step { originCoordinates = p2 (0, -5), name = uniqueName 0 (-5), stepType = EndType }]
 
 connections :: [Step] -> [QDiagram B V2 Double Any -> QDiagram B V2 Double Any]
 connections (x1: x2: xn) = connectOutside' (with & arrowHead .~ noHead) (stepName x1) (stepName x2): connections (x2: xn)
@@ -45,6 +59,7 @@ connections [] = []
 correctShape :: StepType -> Double -> Diagram B
 correctShape StartType = startShape
 correctShape EndType = endShape
+correctShape DecisionStep = decisionShape
 correctShape _ = stepShape
 
 main = mainWith $
--- README.md
+++ README.md
@@ -11,6 +11,17 @@
 
 ![hello-world](./hello-world.svg)
 
+## terminology:
+
+* start - first step of a diagram
+* end - last step of a diagram
+* command - "do X" non-branching action, basic building block of a diagram
+* decision - branching step with two possible outcomes:
+  * yes
+  * no
+
+**todo:** set diagram here? Depending on how I implement the steps.
+
 ## resources
 
 * [drakon](https://drakonhub.com/read/docs)
