95474773bfed479658c444c48c7bc4b9d03dc45a
Piotr Justyna <piotr.justyna@gmail.com>
2024-04-09 09:28:21 +0000

    Pretty printing simple directed graphs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,37 @@
 module Main where
 
 import GHC.Data.Graph.Directed
+import GHC.Utils.Outputable
+import GHC.Utils.Ppr
+import System.IO
 
 node1 :: Node Int String
-node1 = DigraphNode { node_payload = "hello", node_key = 1, node_dependencies = [] }
+node1 = DigraphNode { node_payload = "Buy a puppy", node_key = 1, node_dependencies = [] }
+
+node2 :: Node Int String
+node2 = DigraphNode { node_payload = "Do you have money?", node_key = 2, node_dependencies = [1] }
+
+node3 :: Node Int String
+node3 = DigraphNode { node_payload = "Are puppies on sale today?", node_key = 3, node_dependencies = [2] }
+
+node4 :: Node Int String
+node4 = DigraphNode { node_payload = "Find a puppy", node_key = 4, node_dependencies = [3] }
+
+node5 :: Node Int String
+node5 = DigraphNode { node_payload = "Do you like the puppy?", node_key = 5, node_dependencies = [4] }
+
+node6 :: Node Int String
+node6 = DigraphNode { node_payload = "Buy this cute puppy!", node_key = 6, node_dependencies = [5] }
+
+node7 :: Node Int String
+node7 = DigraphNode { node_payload = "end", node_key = 7, node_dependencies = [6, 5, 3, 2] }
+
+graph :: Graph (Node Int String)
+graph = graphFromEdgedVerticesUniq [node1, node2, node3, node4, node5, node6, node7]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = printSDocLn
+  defaultSDocContext
+  LeftMode
+  stderr
+  (ppr graph)
