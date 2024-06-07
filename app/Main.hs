ea92ab9b7c695bd7d73cc9b135b4d56a6e178b85
Piotr Justyna <piotr.justyna@gmail.com>
2024-06-07 21:36:41 +0000

    parser v2 is now feature matching parser v1

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,5 @@ import qualified ParserV2
 main :: IO ()
 main = do
   print . show $ ParserV2.parse (ParserV2.matchingCharacter 'h') "hello world"
-  print . show $ ParserV2.parse (ParserV2.matchingString "hello") "helloworld"
+  print . show $ ParserV2.parse (ParserV2.matchingString "hello") "hello world"
+  print . show $ ParserV2.parse (ParserV2.symbol "hello") "    hello   world"
--- app/ParserV2.hs
+++ app/ParserV2.hs
@@ -2,6 +2,7 @@ module ParserV2 where
 
 import qualified Control.Applicative
 import qualified Control.Monad
+import qualified Data.Char
 
 newtype Parser a = Parser (String -> Maybe (a, String))
 
@@ -16,23 +17,27 @@ conditionalSingleCharacterParser predicate = Parser (\case
 matchingCharacter :: Char -> Parser Char
 matchingCharacter x = conditionalSingleCharacterParser (== x)
 
--- 2024-06-06 PJ:
--- ==============
--- OK, this...
--- I don't fully understand the do notation here.
--- It just looks too simple, I don't see how it connects
--- to the previously defined functions.
--- 2024-06-06 PJ:
--- ==============
--- The types here are also looking messed up...
--- why return a char/string from matchingCharacter x/matchingString xs
--- if we're not using it.
 matchingString :: String -> Parser String
 matchingString [] = return ""
 matchingString (x:xs) = do
     y <- matchingCharacter x
     ys <- matchingString xs
     return (y:ys)
+
+space :: Parser ()
+space = do
+    _ <- Control.Applicative.many $ conditionalSingleCharacterParser Data.Char.isSpace
+    return ()
+
+token :: Parser a -> Parser a
+token parser = do
+    space
+    value <- parser
+    space
+    return value
+
+symbol :: String -> Parser String
+symbol xs = token $ matchingString xs
 
 instance Functor Parser where
     -- fmap :: (a -> b) -> Parser a -> Parser b
@@ -47,24 +52,10 @@ instance Control.Applicative.Applicative Parser where
         Parser (\x -> Just (applicativeInput, x))
 
     -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    -- 2024-06-06 PJ:
-    -- ==============
-    -- This is rather complicated to me. Need to remember that f is of type:
-    -- String -> Maybe (a -> b, String)
-    -- So it's a parser that can maybe return:
-    -- * another function that can be fmapped over Parser a
-    -- * remaining input
     (<*>) (Parser firstParsingFunction) nextParser =
         Parser (\x -> case firstParsingFunction x of
             Nothing -> Nothing
             Just (firstParsingFunctionResult, remainingInput) -> parse (fmap firstParsingFunctionResult nextParser) remainingInput)
-
-instance Control.Monad.Monad Parser where
-    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    (>>=) (Parser parsingFunction) sequentialAction =
-        Parser (\x -> case parsingFunction x of
-            Nothing -> Nothing
-            Just (parsingFunctionResult, remainingInput) -> parse (sequentialAction parsingFunctionResult) remainingInput)
 
 instance Control.Applicative.Alternative Parser where
     -- empty :: Parser a
@@ -75,3 +66,10 @@ instance Control.Applicative.Alternative Parser where
         Parser (\x -> case parsingFunction1 x of
             Nothing -> parsingFunction2 x
             parsingFunction1Result -> parsingFunction1Result)
+
+instance Control.Monad.Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    (>>=) (Parser parsingFunction) sequentialAction =
+        Parser (\x -> case parsingFunction x of
+            Nothing -> Nothing
+            Just (parsingFunctionResult, remainingInput) -> parse (sequentialAction parsingFunctionResult) remainingInput)
