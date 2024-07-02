fe8f16d6bd17c3f5019a20589d184034b6593968
Piotr Justyna <piotr.justyna@gmail.com>
2024-07-02 20:28:42 +0000

    serialization and deserialization works

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,7 @@ module Main where
 
 import qualified Control.Applicative
 import qualified Data.Aeson
-
-data Coord = Coord { x :: Double, y :: Double }
-  deriving (Show)
-
-instance Data.Aeson.ToJSON Coord where
-  toJSON (Coord x' y') = Data.Aeson.object [ "x" Data.Aeson..= x', "y" Data.Aeson..= y' ]
-
-  toEncoding (Coord x' y') = Data.Aeson.pairs $ "x" Data.Aeson..= x' <> "y" Data.Aeson..= y'
-
-instance Data.Aeson.FromJSON Coord where
-  parseJSON (Data.Aeson.Object v) = Coord <$> v Data.Aeson..: "x" <*> v Data.Aeson..: "y"
-  parseJSON _                     = Control.Applicative.empty
+import qualified Data.Text
 
 data IconType = Title | End | Action | Question
 
@@ -31,16 +20,34 @@ instance Data.Aeson.ToJSON IconType where
   toJSON Action   = Data.Aeson.String "Action"
   toJSON Question = Data.Aeson.String "Question"
 
+instance Data.Aeson.FromJSON IconType where
+  parseJSON = Data.Aeson.withText "iconType" $ \t -> case Data.Text.unpack t of
+    "Title"     -> pure Title
+    "End"       -> pure End
+    "Action"    -> pure Action
+    "Question"  -> pure Question
+    unknown -> fail $ "unknown iconType: " <> unknown
+
 data Icon = Icon { iconText :: String, iconType :: IconType }
   deriving (Show)
 
 instance Data.Aeson.ToJSON Icon where
   toJSON (Icon iconText' iconType') = Data.Aeson.object [ "iconText" Data.Aeson..= iconText', "iconType" Data.Aeson..= iconType' ]
 
+instance Data.Aeson.FromJSON Icon where
+  parseJSON (Data.Aeson.Object v) = Icon <$> v Data.Aeson..: "iconText" <*> v Data.Aeson..: "iconType"
+  parseJSON _                     = Control.Applicative.empty
+
 main :: IO ()
 main = do
   let icon = Icon { iconText = "text", iconType = Title }
   print icon
-  print $ Data.Aeson.encode icon
-  let req = Data.Aeson.decode "{\"x\":3.0,\"y\":-1.0}" :: Maybe Coord
-  print req
+
+  let serializedIcon = Data.Aeson.encode icon
+  print serializedIcon
+
+  let icon' = Data.Aeson.decode serializedIcon :: Maybe Icon
+  print icon'
+
+  let problematicIcon = Data.Aeson.decode "{\"iconText\":\"text\",\"iconType\":\"Title\"}" :: Maybe Icon
+  print problematicIcon
