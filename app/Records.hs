6e18d8306587b83ef9b63309c6a97ecbfbedc2f9
Piotr Justyna <piotr.justyna@gmail.com>
2024-07-31 15:05:22 +0000

    Icon type has dependencies now.

--- app/Records.hs
+++ app/Records.hs
@@ -8,26 +8,36 @@ import qualified DataTypes
 import qualified GHC.Utils.Outputable
 
 data Icon = Icon {
-  iconName        :: String,
-  iconDescription :: String,
-  iconKind        :: DataTypes.IconKind }
+  iconName                  :: String,
+  iconDescription           :: String,
+  iconNamesOfDependentIcons :: [String],
+  iconKind                  :: DataTypes.IconKind }
     deriving (Show)
+
+getIconName :: Icon -> String
+getIconName Icon {
+  iconName = x,
+  iconDescription = _,
+  iconNamesOfDependentIcons = _,
+  iconKind = _ } = x
 
 instance GHC.Utils.Outputable.Outputable Icon where
     ppr icon = GHC.Utils.Outputable.text $ show icon
 
 instance Data.Aeson.ToJSON Icon where
-  toJSON (Icon name description kind) =
+  toJSON (Icon name description kind namesOfDependentIcons) =
     Data.Aeson.object [
       "iconName" Data.Aeson..= name,
       "iconDescription" Data.Aeson..= description,
-      "iconKind" Data.Aeson..= kind]
+      "iconKind" Data.Aeson..= kind,
+      "iconNamesOfDependentIcons" Data.Aeson..= namesOfDependentIcons]
 
 instance Data.Aeson.FromJSON Icon where
   parseJSON (Data.Aeson.Object v) =
     Icon <$>
       v Data.Aeson..: "iconName" <*>
       v Data.Aeson..: "iconDescription" <*>
-      v Data.Aeson..: "iconKind"
+      v Data.Aeson..: "iconKind" <*>
+      v Data.Aeson..: "iconNamesOfDependentIcons"
   parseJSON _                     =
     Control.Applicative.empty
