d54049843527d38bcf91afa63b41fb956e971e62
Piotr Justyna <piotr.justyna@gmail.com>
2024-08-06 09:19:42 +0000

    basic exception handling on diagram input

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,18 @@ module Main where
 
 import qualified Data.Aeson
 import qualified Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy.Char8
 import qualified GHC.Data.FastString
 import qualified GHC.Data.Graph.Directed
 import qualified GHC.Utils.Outputable
 import qualified GHC.Utils.Ppr
 import qualified Records
+import qualified System.Directory
 import qualified System.IO
+import qualified Control.Exception
+
+maxInputFileSizeInBytes :: Integer
+maxInputFileSizeInBytes = 102400
 
 directedGraph ::
   [Records.Icon] ->
@@ -26,16 +32,29 @@ directedGraph icons =
         GHC.Data.Graph.Directed.node_dependencies = GHC.Data.FastString.fsLit <$> Records.getIconNamesOfDependentIcons icon }
         | icon <- icons]
 
+handleReadError ::
+  Control.Exception.IOException ->
+  IO Data.ByteString.Lazy.ByteString
+handleReadError e = return . Data.ByteString.Lazy.Char8.pack $ "Error reading file: " ++ show e
+
 main :: IO ()
 main = do
-  let filePath = "./diagrams/drakon-diagram-1.json"
+  let diagramFilePath = "./diagrams/drakon-diagram-1.json"
 
-  content <- Data.ByteString.Lazy.readFile filePath
+  fileSizeInBytes <- System.Directory.getFileSize diagramFilePath
 
-  case Data.Aeson.decode content :: Maybe [Records.Icon] of
-    Just icons -> do
-      GHC.Utils.Outputable.printSDocLn
-        GHC.Utils.Outputable.defaultSDocContext
-        GHC.Utils.Ppr.LeftMode
-        System.IO.stdout . GHC.Utils.Outputable.ppr $ directedGraph icons
-    Nothing -> return ()
+  if fileSizeInBytes > maxInputFileSizeInBytes
+    then
+      putStrLn $ "Problem with diagram file \"" ++ diagramFilePath ++ "\" (" ++ show fileSizeInBytes ++ " bytes). Max allowed input file size: " ++ show maxInputFileSizeInBytes ++ " bytes."
+    else do
+      content <- Control.Exception.catch (Data.ByteString.Lazy.readFile diagramFilePath) handleReadError
+
+      case Data.Aeson.decode content :: Maybe [Records.Icon] of
+        Just icons -> do
+          GHC.Utils.Outputable.printSDocLn
+            GHC.Utils.Outputable.defaultSDocContext
+            GHC.Utils.Ppr.LeftMode
+            System.IO.stdout . GHC.Utils.Outputable.ppr $ directedGraph icons
+        Nothing -> do
+          let unpackedContent = Data.ByteString.Lazy.Char8.unpack content
+          putStrLn $ "Problem decoding diagram file \"" ++ diagramFilePath ++ "\". Details: " ++ unpackedContent
