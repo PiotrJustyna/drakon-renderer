0c583d8aecfdc98c36984c8735afe187156b7bfb
Piotr Justyna <piotr.justyna@gmail.com>
2024-08-16 10:20:23 +0000

    barebones exploratory layout

--- app/LayoutEngine.hs
+++ app/LayoutEngine.hs
@@ -19,14 +19,54 @@ cartesianPositioning x =
 
 exploratoryCartesianPositioning :: Int -> Int -> GHC.Data.Graph.Directed.Node GHC.Data.FastString.FastString Records.Icon -> [GHC.Data.Graph.Directed.Node GHC.Data.FastString.FastString Records.Icon] -> [Records.PositionedIcon]
 exploratoryCartesianPositioning x y n ns =
+  [Records.PositionedIcon {
+    Records.icon = Records.payload n,
+    Records.iconPositionX = x,
+    Records.iconPositionY = y }]
+  where
+    dependentNodes = Records.nodesIdentifiedWithKeys ns $ Records.dependencies n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exploratoryCartesianPositioning' :: Int -> Int -> GHC.Data.Graph.Directed.Node GHC.Data.FastString.FastString Records.Icon -> [GHC.Data.Graph.Directed.Node GHC.Data.FastString.FastString Records.Icon] -> [Records.PositionedIcon]
+exploratoryCartesianPositioning' x y n ns =
   Records.PositionedIcon {
     Records.icon = Records.payload n,
     Records.iconPositionX = x,
-    Records.iconPositionY = y } : concat [exploratoryCartesianPositioning (x + index) (y - 1) dependentNode ns | (index, dependentNode) <- indexedDependentNodes]
+    Records.iconPositionY = y } : foldl (\acc (index, dependentNode) -> acc ++ exploratoryCartesianPositioning' (x + index) (y - 1) dependentNode ns) [] indexedDependentNodes
   where
     nodeKeysOfDependentNodes = Records.dependencies n
-    dependentNodes = Records.nodesIdentifiedWithKeys ns nodeKeysOfDependentNodes
+    dependentNodes = reverse $ Records.nodesIdentifiedWithKeys ns nodeKeysOfDependentNodes
     indexedDependentNodes = zip [0 ..] dependentNodes :: [(Int, GHC.Data.Graph.Directed.Node GHC.Data.FastString.FastString Records.Icon)]
 
 removeDuplicates :: [Records.PositionedIcon] -> [Records.PositionedIcon]
 removeDuplicates = reverse . foldl (\acc x -> if any (\y -> Records.getPositionedIconName y == Records.getPositionedIconName x) acc then acc else x:acc) []
+
+-- pushPositionedIcons :: [Records.PositionedIcon] -> [Records.PositionedIcon]
+-- pushPositionedIcons allPositionedIcons = foldr (\x acc -> if collidesWithAnotherIcon x allPositionedIcons then acc else x:acc) [] allPositionedIcons
+
+-- collidesWithAnotherIcon :: Records.PositionedIcon -> [Records.PositionedIcon] -> Bool
+-- collidesWithAnotherIcon singlePositonedIcon allPositionedIcons = any (\x -> Records.collision x singlePositonedIcon) allPositionedIcons
