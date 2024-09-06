c0db4b3f08de38cb3d9e8f9b4b967dd8c5ec0af9
Piotr Justyna <piotr.justyna@gmail.com>
2024-09-06 15:54:52 +0000

    corrected dependency positioning

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cabal run drakon-renderer -- \
-    --textInputPath "./diagrams/drakon-diagram.json" \
-    --textOutputPath "./diagrams/drakon-diagram-layout.json" \
-    --svgOutputPath "./diagrams/drakon-diagram.svg"
+    --textInputPath "./diagrams/real-life-diagram-1.json" \
+    --textOutputPath "./diagrams/real-life-diagram-1-layout.json" \
+    --svgOutputPath "./diagrams/real-life-diagram-1-diagram.svg"
