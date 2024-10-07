915ccbc7e9a451d8bfd2825b252655d46f81553c
Piotr Justyna <piotr.justyna@gmail.com>
2024-10-07 19:49:21 +0000

    loop back almost working - only need an arrow

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-cabal run drakon-renderer -- \
-    --textInputPath "./diagrams/real-life-diagram-1.json" \
-    --textOutputPath "./diagrams/real-life-diagram-1-layout.json" \
-    --svgOutputPath "./diagrams/real-life-diagram-1.svg"
+# cabal run drakon-renderer -- \
+#     --textInputPath "./diagrams/real-life-diagram-1.json" \
+#     --textOutputPath "./diagrams/real-life-diagram-1-layout.json" \
+#     --svgOutputPath "./diagrams/real-life-diagram-1.svg"
+
+# cabal run drakon-renderer -- \
+#     --textInputPath "./diagrams/drakon-diagram.json" \
+#     --textOutputPath "./diagrams/drakon-diagram-layout.json" \
+#     --svgOutputPath "./diagrams/drakon-diagram.svg"
 
 cabal run drakon-renderer -- \
-    --textInputPath "./diagrams/drakon-diagram.json" \
-    --textOutputPath "./diagrams/drakon-diagram-layout.json" \
-    --svgOutputPath "./diagrams/drakon-diagram.svg"
+    --textInputPath "./diagrams/primitive-diagram-1.json" \
+    --textOutputPath "./diagrams/primitive-diagram-1-layout.json" \
+    --svgOutputPath "./diagrams/primitive-diagram-1.svg"
