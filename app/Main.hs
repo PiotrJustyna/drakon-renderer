26cd5a81b1b8af8866ffb9c407e7eef42201e44d
Piotr Justyna <piotr.justyna@gmail.com>
2025-01-14 10:32:17 +0000

    visual representation of terminators and skewer blocks

--- app/Main.hs
+++ app/Main.hs
@@ -8,29 +8,111 @@ data Terminator
   | CyclicStartWithParameters
 
 instance Show Terminator where
-  show Title = "T"
-  show CyclicStart = "CS"
-  show End = "E"
-  show TitleWithParameters = "TWP"
-  show CyclicStartWithParameters = "CSWP"
+  show Title = visualBlockNoEntry "Title"
+  show CyclicStart = visualBlockNoEntry "CyclicStart"
+  show End = visualBlockNoExit "End"
+  show TitleWithParameters = visualBlockNoEntry "TitleWithParameters"
+  show CyclicStartWithParameters = visualBlockNoEntry "CyclicStartWithParameters"
 
-data Block
-  = TerminatorBlock Terminator
-  | SkewerBlock
+data SkewerBlock
+  = Headline
+  | Address
+  | Action
+  | Shelf
+  | Choice
+  | Case
+  | BeginOfForLoop
+  | EndOfForLoop
+  | Output
+  | Input
+  | Insertion
+  | Pause
+  | StartTimer
+  | ParallelProcess
+  | Comment
 
-instance Show Block where
-  show SkewerBlock = "|"
-  show (TerminatorBlock t) = show t
+instance Show SkewerBlock where
+  show Headline = visualBlock "Headline"
+  show Address = visualBlock "Address"
+  show Action = visualBlock "Action"
+  show Shelf = visualBlock "Shelf"
+  show Choice = visualBlock "Choice"
+  show Case = visualBlock "Case"
+  show BeginOfForLoop = visualBlock "BeginOfForLoop"
+  show EndOfForLoop = visualBlock "EndOfForLoop"
+  show Output = visualBlock "Output"
+  show Input = visualBlock "Input"
+  show Insertion = visualBlock "Insertion"
+  show Pause = visualBlock "Pause"
+  show StartTimer = visualBlock "StartTimer"
+  show ParallelProcess = visualBlock "ParallelProcess"
+  show Comment = visualBlock "Comment"
+
+data DiagramBlock
+  = TerminatorDiagramBlock Terminator
+  | SkewerDiagramBlock SkewerBlock
+
+instance Show DiagramBlock where
+  show (SkewerDiagramBlock x) = show x
+  show (TerminatorDiagramBlock x) = show x
 
 newtype Diagram = Diagram
-  { blocks :: [Block]
+  { blocks :: [DiagramBlock]
   }
 
 instance Show Diagram where
   show Diagram {blocks = x} = foldl (\accu singleBlock -> accu <> "\n" <> show singleBlock) "" x
 
+visualBlockLength :: Int
+visualBlockLength = 21
+
+fill :: Int -> Char -> String
+fill 0 _ = ""
+fill limit fillingCharacter = fillingCharacter : fill (limit - 1) fillingCharacter
+
+visualBlockCore :: String -> String
+visualBlockCore content =
+    fill visualBlockLength '-'
+    <> "\n| "
+    <> content
+    <> fill (visualBlockLength - 2 - 1 - length content) ' '
+    <> "|\n"
+    <> fill visualBlockLength '-'
+
+visualBlock :: String -> String
+visualBlock content =
+    fill (visualBlockLength `div` 2) ' '
+    <> "|\n"
+    <> visualBlockCore content
+    <> "\n"
+    <> fill (visualBlockLength `div` 2) ' '
+    <> "|"
+
+visualBlockNoEntry :: String -> String
+visualBlockNoEntry content =
+    visualBlockCore content
+    <> "\n"
+    <> fill (visualBlockLength `div` 2) ' '
+    <> "|"
+
+visualBlockNoExit :: String -> String
+visualBlockNoExit content =
+    fill (visualBlockLength `div` 2) ' '
+    <> "|\n"
+    <> visualBlockCore content
+
 sampleDiagram :: Diagram
-sampleDiagram = Diagram {blocks = (TerminatorBlock Title) : SkewerBlock : SkewerBlock : [(TerminatorBlock End)]}
+sampleDiagram =
+  Diagram
+    { blocks =
+        TerminatorDiagramBlock Title
+          : SkewerDiagramBlock Action
+          : SkewerDiagramBlock BeginOfForLoop
+          : SkewerDiagramBlock EndOfForLoop
+          : SkewerDiagramBlock ParallelProcess
+          : SkewerDiagramBlock Action
+          : [TerminatorDiagramBlock End]
+    }
 
 main :: IO ()
 main = do
