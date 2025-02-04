61248cafea327f408153c45235274393288707f2
Piotr Justyna <piotr.justyna@gmail.com>
2025-02-04 10:35:54 +0000

    simple input parser

--- app/Main_NewRendering.hs
+++ app/Main_NewRendering.hs
@@ -105,6 +105,14 @@ data SkewerBlock
   | Question
   | Fork Branch Branch
 
+instance Show DrakonDiagram where
+  show diagram =
+    "diagram total width in units: "
+      <> show (widthInUnits diagram)
+      <> "\n"
+      <> "diagram total height in units: "
+      <> show (heightInUnits diagram)
+
 instance Renderer StartTerminator where
   render Title origin =
     position
@@ -185,28 +193,27 @@ instance Renderer SkewerBlock where
   render fork@(Fork l r) origin@(P (V2 x y)) =
     render Question origin
       <> render
-          l
-          (P (V2 x (y - heightInUnits Question * defaultBoundingBoxHeight)))
+           l
+           (P (V2 x (y - heightInUnits Question * defaultBoundingBoxHeight)))
       <> render
-          r
-          (P (V2
-                (x + widthInUnits l * defaultBoundingBoxWidth)
-                (y - heightInUnits Question * defaultBoundingBoxHeight)))
+           r
+           (P (V2
+                 (x + widthInUnits l * defaultBoundingBoxWidth)
+                 (y - heightInUnits Question * defaultBoundingBoxHeight)))
       <> position
-          [ ( origin
-            , rect'
-                (widthInUnits fork * defaultBoundingBoxWidth)
-                (heightInUnits fork * defaultBoundingBoxHeight)
-                # lw veryThin)
-          ]
-
+           [ ( origin
+             , rect'
+                 (widthInUnits fork * defaultBoundingBoxWidth)
+                 (heightInUnits fork * defaultBoundingBoxHeight)
+                 # lw veryThin)
+           ]
   widthInUnits Action = 1.0
   widthInUnits Question = 1.0
   widthInUnits (Fork l r) = widthInUnits l + widthInUnits r
-
   heightInUnits Action = 1.0
   heightInUnits Question = 1.0
-  heightInUnits (Fork l r) = heightInUnits Question + max (heightInUnits l) (heightInUnits r)
+  heightInUnits (Fork l r) =
+    heightInUnits Question + max (heightInUnits l) (heightInUnits r)
 
 instance Renderer Branch where
   render (EmptyBranch _) _origin = render ValentPoint _origin
@@ -238,25 +245,90 @@ instance Renderer DrakonDiagram where
           <> fst renderedSkewerBlocks
           <> render finishTerminator (P (V2 x (snd renderedSkewerBlocks)))
   widthInUnits (DrakonDiagram startTerminator skewerBlocks finishTerminator) =
-    maximum $ widthInUnits startTerminator : map widthInUnits skewerBlocks ++ [widthInUnits finishTerminator]
+    maximum
+      $ widthInUnits startTerminator
+          : map widthInUnits skewerBlocks
+          ++ [widthInUnits finishTerminator]
   heightInUnits (DrakonDiagram startTerminator skewerBlocks finishTerminator) =
-    sum $ heightInUnits startTerminator : map heightInUnits skewerBlocks ++ [heightInUnits finishTerminator]
+    sum
+      $ heightInUnits startTerminator
+          : map heightInUnits skewerBlocks
+          ++ [heightInUnits finishTerminator]
+
+parse :: String -> Either String DrakonDiagram
+parse x =
+  case parse' (words x) of
+    Left error -> Left error
+    Right (diagram, []) -> Right diagram
+    Right (diagram, moreTokens) ->
+      Left $ "unexpected tokens: " <> unwords moreTokens
+
+parse' :: [String] -> Either String (DrakonDiagram, [String])
+parse' [] = Left "unexpected end of expression"
+parse' (t:ts) =
+  case t of
+    "Title" ->
+      case parseSkewerBlocks ts of
+        Left error -> Left error
+        Right (skewerBlocks, ts') ->
+          case parseFinishTerminator ts' of
+            Left e -> Left e
+            Right (finishTerminator, ts'') ->
+              Right (DrakonDiagram Title skewerBlocks finishTerminator, [])
+    _ -> Left $ "unexpected token 1: " <> t
+
+parseSkewerBlocks :: [String] -> Either String ([SkewerBlock], [String])
+parseSkewerBlocks (t:ts) = case t of
+  "[" -> combine . loop $ parseSkewerBlock ts
+  _ -> Left $ "unexpected token 2: " <> t
+
+loop :: Either String (Maybe SkewerBlock, [String]) -> [Either String (Maybe SkewerBlock, [String])]
+loop (Left e) = [Left e]
+loop x@(Right (Nothing, ts)) = [x]
+loop x@(Right (Just _, ts)) = x : loop (parseSkewerBlock ts)
+
+combine :: [Either String (Maybe SkewerBlock, [String])] -> Either String ([SkewerBlock], [String])
+combine = foldl
+  (\accu x -> case accu of
+    Left e -> Left e
+    Right (skewerBlocks, _) ->
+      case x of
+        Left e -> Left e
+        Right (Nothing, ts') -> Right (skewerBlocks, ts')
+        Right (Just skewerBlock, ts') -> Right (skewerBlock : skewerBlocks, ts'))
+  (Right ([], []))
+
+parseSkewerBlock :: [String] -> Either String (Maybe SkewerBlock, [String])
+parseSkewerBlock (t:ts) =
+  case t of
+    "Action" -> Right (Just Action, ts)
+    "Question" -> Right (Just Question, ts)
+    "]" -> Right (Nothing, ts)
+    _ -> Left $ "unexpected token 3: " <> t
+
+parseFinishTerminator :: [String] -> Either String (FinishTerminator, [String])
+parseFinishTerminator (t:ts) =
+  case t of
+    "End" -> Right (End, ts)
+    _ -> Left $ "unexpected token 4: " <> t
 
 main :: IO ()
 main = do
-  let diagram =
-        DrakonDiagram
-          Title
-          [ Action
-          , Fork
-              (FullBranch
-                [ Action
-                , Fork
-                    (FullBranch [Action, Action])
-                    (FullBranch [Action, Action, Action])
-                , Action])
-              (FullBranch [Action, Action, Action, Action])]
-          End
-  putStrLn $ "diagram total width in units: " <> show (widthInUnits diagram)
-  putStrLn $ "diagram total height in units: " <> show (heightInUnits diagram)
-  renderSVG' svgOutputPath svgOptions $ render diagram (p2 (0.0, 0.0))
+  case parse "Title [ Action Action ] End" of
+    Left error -> putStrLn error
+    Right diagram -> do
+      print diagram
+      renderSVG' svgOutputPath svgOptions $ render diagram (p2 (0.0, 0.0))
+  -- let diagram =
+  --       DrakonDiagram
+  --         Title
+  --         [ Action
+  --         , Fork
+  --             (FullBranch
+  --               [ Action
+  --               , Fork
+  --                   (FullBranch [Action, Action])
+  --                   (FullBranch [Action, Action, Action])
+  --               , Action])
+  --             (FullBranch [Action, Action, Action, Action])]
+  --         End
