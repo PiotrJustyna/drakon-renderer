f17326238ba61aa71025e2fcfa8799a55fcd31a6
Piotr Justyna <piotr.justyna@gmail.com>
2025-02-05 12:36:41 +0000

    cleared up all warnings

--- app/Main_NewRendering.hs
+++ app/Main_NewRendering.hs
@@ -150,7 +150,7 @@ instance Renderer ValentPoint where
   heightInUnits _ = 1.0
 
 render' :: [SkewerBlock] -> Point V2 Double -> Diagram B
-render' skewerBlocks origin@(P (V2 x y)) =
+render' skewerBlocks (P (V2 x y)) =
   fst
     $ foldl
         (\accu singleBlock ->
@@ -205,11 +205,11 @@ instance Renderer SkewerBlock where
   render fork@(Fork l r) origin@(P (V2 x y)) =
     render Question origin
       <> if null l
-           then (render ValentPoint lOrigin)
-           else (render' l lOrigin)
+           then render ValentPoint lOrigin
+           else render' l lOrigin
                   <> if null r
-                       then (render ValentPoint rOrigin)
-                       else (render' r rOrigin)
+                       then render ValentPoint rOrigin
+                       else render' r rOrigin
                               <> position
                                    [ ( origin
                                      , rect'
@@ -274,9 +274,9 @@ instance Renderer DrakonDiagram where
 parse :: String -> Either String DrakonDiagram
 parse x =
   case parse' (words x) of
-    Left error -> Left error
+    Left e -> Left e
     Right (diagram, []) -> Right diagram
-    Right (diagram, moreTokens) ->
+    Right (_, moreTokens) ->
       Left $ "unexpected tokens: " <> unwords moreTokens
 
 parse' :: [String] -> Either String (DrakonDiagram, [String])
@@ -285,19 +285,20 @@ parse' (t:ts) =
   case t of
     "Title" ->
       case parseSkewerBlocks ts of
-        Left error -> Left error
+        Left e -> Left e
         Right (skewerBlocks, ts') ->
           case parseFinishTerminator ts' of
             Left e -> Left e
-            Right (finishTerminator, ts'') ->
+            Right (finishTerminator, _) ->
               Right (DrakonDiagram Title skewerBlocks finishTerminator, [])
-    _ -> Left $ "unexpected token 1: " <> t
+    _ -> Left $ "unexpected token: " <> t
 
 parseSkewerBlocks :: [String] -> Either String ([SkewerBlock], [String])
+parseSkewerBlocks [] = Left "no skewer block tokens provided"
 parseSkewerBlocks (t:ts) =
   case t of
     "[" -> combine . loop $ parseSkewerBlock ts
-    _ -> Left $ "unexpected token 2: " <> t
+    _ -> Left $ "unexpected token: " <> t
 
 loop ::
      Either String (Maybe SkewerBlock, [String])
@@ -323,23 +324,25 @@ combine =
     (Right ([], []))
 
 parseSkewerBlock :: [String] -> Either String (Maybe SkewerBlock, [String])
+parseSkewerBlock [] = Left "no skewer block tokens provided"
 parseSkewerBlock (t:ts) =
   case t of
     "Action" -> Right (Just Action, ts)
     "Question" -> Right (Just Question, ts)
     "]" -> Right (Nothing, ts)
-    _ -> Left $ "unexpected token 3: " <> t
+    _ -> Left $ "unexpected token: " <> t
 
 parseFinishTerminator :: [String] -> Either String (FinishTerminator, [String])
+parseFinishTerminator [] = Left "no finish terminator tokens provided"
 parseFinishTerminator (t:ts) =
   case t of
     "End" -> Right (End, ts)
-    _ -> Left $ "unexpected token 4: " <> t
+    _ -> Left $ "unexpected token: " <> t
 
 main :: IO ()
 main = do
   case parse "Title [ Action Action Question Action ] End" of
-    Left error -> putStrLn error
+    Left e -> putStrLn e
     Right diagram -> do
       print diagram
       renderSVG' svgOutputPath svgOptions $ render diagram (p2 (0.0, 0.0))
