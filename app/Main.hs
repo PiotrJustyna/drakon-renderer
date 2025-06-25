2898db107441674540424091727245f6a4813c49
Piotr Justyna <piotr.justyna@gmail.com>
2025-06-25 08:25:06 +0000

    introduced happy, added happy and alex into the build script

--- app/Main.hs
+++ app/Main.hs
@@ -11,13 +11,6 @@ import Drakon.SkewerBlock (ConnectedSkewerBlocks(ConnectedSkewerBlocks), SkewerB
 import Drakon.StartTerminator (StartTerminator(Title))
 import Lexer (alexScanTokens)
 
--- parse :: String -> Either String DrakonDiagram
--- parse x =
---   case parse' (words x) of
---     Left e -> Left e
---     Right (diagram, []) -> Right diagram
---     Right (_, moreTokens) -> Left $ "unexpected tokens: " <> unwords moreTokens
-
 -- 2025-06-19 PJ:
 -- --------------
 -- For now the assumption is that we are only
@@ -72,106 +65,8 @@ parseSkewerBlock' (t:ts) = Right (Just (Action (ID "-1") (p2 (-1.0, -1.0)) (Cont
 parseEndTerminator :: (String, String) -> Either String EndTerminator
 parseEndTerminator (x, y) = Right (End (ID "-1") (p2 (-1.0, -1.0)) (Content "custom content - end"))
 
--- parseSkewerBlock :: [String] -> Either String (Maybe SkewerBlock, [String])
--- parseSkewerBlock [] = Left "no skewer block tokens provided"
--- parseSkewerBlock (t:ts) =
---   case t of
---     "Action" -> Right (Just (Action (ID "-1") (p2 (-1.0, -1.0)) (Content "custom content - action")), ts)
---     "Fork" ->
---       case parseSkewerBlocks ts of
---         Left e -> Left e
---         Right (lSkewerBlocks, ts') ->
---           case parseSkewerBlocks ts' of
---             Left e -> Left e
---             Right (rSkewerBlocks, ts'') ->
---               Right
---                 ( Just
---                     (Fork
---                        (ID "-1")
---                        (p2 (-1.0, -1.0))
---                        (Content "custom content - fork")
---                        (ConnectedSkewerBlocks lSkewerBlocks Nothing)
---                        (ConnectedSkewerBlocks rSkewerBlocks Nothing))
---                 , ts'')
---     "]" -> Right (Nothing, ts)
---     _ -> Left $ "unexpected token: " <> t
-
 main :: IO ()
 main = do
-  -- let newDiagram@(DrakonDiagram _ _ _) =
-  --       -- silhouette diagram 1:
-  --       DrakonDiagram
-  --         (Title (ID "100") (p2 (-1.0, -1.0)) (Content "bus journey"))
-  --         [ [ Headline (ID "101") (p2 (-1.0, -1.0)) (Content "skewer 1 headline")
-  --           , Action (ID "200") (p2 (-1.0, -1.0)) (Content "find a bus stop")
-  --           , Fork
-  --               (ID "300")
-  --               (p2 (-1.0, -1.0))
-  --               (Content "has a bus arrived?")
-  --               (ConnectedSkewerBlocks [Action (ID "400") (p2 (-1.0, -1.0)) (Content "passengers boarding")] Nothing)
-  --               (ConnectedSkewerBlocks [] (Just (ID "500")))
-  --           , Fork
-  --               (ID "500")
-  --               (p2 (-1.0, -1.0))
-  --               (Content "is it your turn?")
-  --               (ConnectedSkewerBlocks [] Nothing)
-  --               (ConnectedSkewerBlocks
-  --                  [Action (ID "610") (p2 (-1.0, -1.0)) (Content "wait for your turn")]
-  --                  (Just (ID "500")))
-  --           , Fork
-  --               (ID "700")
-  --               (p2 (-1.0, -1.0))
-  --               (Content "is it possible to enter the bus?")
-  --               (ConnectedSkewerBlocks [Action (ID "800") (p2 (-1.0, -1.0)) (Content "enter the bus")] Nothing)
-  --               (ConnectedSkewerBlocks [] (Just (ID "410")))
-  --           , Fork
-  --               (ID "900")
-  --               (p2 (-1.0, -1.0))
-  --               (Content "are any seats available?")
-  --               (ConnectedSkewerBlocks [Action (ID "1000") (p2 (-1.0, -1.0)) (Content "take a seat")] Nothing)
-  --               (ConnectedSkewerBlocks
-  --                  [ Fork
-  --                      (ID "1010")
-  --                      (p2 (-1.0, -1.0))
-  --                      (Content "do you want to travel standing?")
-  --                      (ConnectedSkewerBlocks [] Nothing)
-  --                      (ConnectedSkewerBlocks
-  --                         [ Action (ID "1010") (p2 (-1.0, -1.0)) (Content "leave the bus")
-  --                         , Action (ID "410") (p2 (-1.0, -1.0)) (Content "wait for the next bus")
-  --                         ]
-  --                         (Just (ID "300")))
-  --                  ]
-  --                  Nothing)
-  --           , Fork
-  --               (ID "1200")
-  --               (p2 (-1.0, -1.0))
-  --               (Content "do you have money for a ticket?")
-  --               (ConnectedSkewerBlocks [Action (ID "1300") (p2 (-1.0, -1.0)) (Content "buy a ticket")] Nothing)
-  --               (ConnectedSkewerBlocks [] Nothing)
-  --           , Action (ID "1400") (p2 (-1.0, -1.0)) (Content "travel to the required stop")
-  --           , Action (ID "1500") (p2 (-1.0, -1.0)) (Content "leave the bus")
-  --           , Address (ID "1501") (p2 (-1.0, -1.0)) (Content "skewer 1 address")
-  --           ]
-  --         , [ Headline (ID "1599") (p2 (-1.0, -1.0)) (Content "skewer 2 headline")
-  --           , Action (ID "1600") (p2 (-1.0, -1.0)) (Content "skewer 2 action")
-  --           , Address (ID "1700") (p2 (-1.0, -1.0)) (Content "skewer 2 address")
-  --           ]
-  --         , [ Headline (ID "1599a") (p2 (-1.0, -1.0)) (Content "skewer 3 headline")
-  --           , Action (ID "1600a") (p2 (-1.0, -1.0)) (Content "skewer 3 action")
-  --           , Action (ID "1601a") (p2 (-1.0, -1.0)) (Content "skewer 3 action")
-  --           , Address (ID "1700a") (p2 (-1.0, -1.0)) (Content "skewer 3 address")
-  --           ]
-  --         , [ Headline (ID "1599b") (p2 (-1.0, -1.0)) (Content "skewer 4 headline")
-  --           , Action (ID "1600b") (p2 (-1.0, -1.0)) (Content "skewer 4 action")
-  --           , Action (ID "1601b") (p2 (-1.0, -1.0)) (Content "skewer 4 action")
-  --           , Action (ID "1602b") (p2 (-1.0, -1.0)) (Content "skewer 4 action")
-  --           ]
-  --         ]
-  --         (End (ID "1000000") (p2 (-1.0, -1.0)) (Content "end"))
-  -- print $ Drakon.DrakonDiagram.heightInUnits newDiagram
-  -- print $ Drakon.DrakonDiagram.widthInUnits newDiagram
-  -- let addressY = (-1.0) * Drakon.DrakonDiagram.heightInUnits newDiagram + defaultBoundingBoxHeight * 2.0 -- ignore the heights of start and end terminators
-  -- renderSVG' svgOutputPath svgOptions $ Drakon.DrakonDiagram.render newDiagram addressY
   fileContent <- readFile "./app/Drakon/input.txt"
   let possiblyDiagram = parse $ alexScanTokens fileContent
 
