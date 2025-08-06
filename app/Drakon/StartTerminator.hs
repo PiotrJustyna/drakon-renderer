7fc075df3728150fdbd7fcb17efd7348b49a3dcd
Piotr Justyna <piotr.justyna@gmail.com>
2025-08-06 10:44:25 +0000

    removed the renderer typeclass

--- app/Drakon/StartTerminator.hs
+++ app/Drakon/StartTerminator.hs
@@ -1,34 +1,21 @@
 module Drakon.StartTerminator where
 
+import Data.Map (Map)
 import Diagrams.Backend.SVG (B)
 import Diagrams.Prelude
   ( Diagram
   , Point
   , V2(..)
   , (#)
-  , closeLine
-  , fc
-  , font
-  , fontSize
-  , fromOffsets
-  , lc
-  , light
-  , local
-  , lw
-  , origin
   , position
   , r2
   , roundedRect
-  , strokeLoop
-  , text
   , translate
-  , veryThin
   )
 import Drakon.Constants
 import Drakon.Content (Content(..))
 import Drakon.HelperDiagrams
 import Drakon.ID (ID)
-import Drakon.TypeClasses
 
 data StartTerminator
   = Title ID (Point V2 Double) Content
@@ -44,29 +31,33 @@ changeOrigin (TitleWithParameters startId _ content) newOrigin = TitleWithParame
 changeOrigin (CyclicStartWithParameters startId _ content) newOrigin =
   CyclicStartWithParameters startId newOrigin content
 
-instance Renderer StartTerminator where
-  render title@(Title titleId origin (Content content)) _ =
-    position
-      [ ( origin
-        , renderText
-            ((if troubleshootingMode
-                then "[" <> show titleId <> " | " <> show origin <> "] "
-                else "")
-               <> content)
-            (0.0 + widthInUnits title * defaultBoundingBoxWidth * 0.5)
-            (0.0 - heightInUnits title * defaultBoundingBoxHeight * 0.5)
-            <> (drakonStyle
-                  (roundedRect
-                     (widthInUnits title * defaultBoundingBoxWidth * widthRatio)
-                     (heightInUnits title * defaultBoundingBoxHeight * 0.5)
-                     0.5)
-                  # translate (r2 (defaultBoundingBoxWidth * 0.5, defaultBoundingBoxHeight * (-0.5))))
-            <> if troubleshootingMode
-                 then boundingBox
-                        (widthInUnits title * defaultBoundingBoxWidth)
-                        (heightInUnits title * defaultBoundingBoxHeight)
-                 else mempty)
-      ]
-  render _ _ = mempty
-  widthInUnits _ = 1.0
-  heightInUnits _ = 1.0
+render :: StartTerminator -> Map ID (Point V2 Double) -> Diagram B
+render title@(Title titleId origin (Content content)) _ =
+  position
+    [ ( origin
+      , renderText
+          ((if troubleshootingMode
+              then "[" <> show titleId <> " | " <> show origin <> "] "
+              else "")
+              <> content)
+          (0.0 + widthInUnits title * defaultBoundingBoxWidth * 0.5)
+          (0.0 - heightInUnits title * defaultBoundingBoxHeight * 0.5)
+          <> (drakonStyle
+                (roundedRect
+                    (widthInUnits title * defaultBoundingBoxWidth * widthRatio)
+                    (heightInUnits title * defaultBoundingBoxHeight * 0.5)
+                    0.5)
+                # translate (r2 (defaultBoundingBoxWidth * 0.5, defaultBoundingBoxHeight * (-0.5))))
+          <> if troubleshootingMode
+                then boundingBox
+                      (widthInUnits title * defaultBoundingBoxWidth)
+                      (heightInUnits title * defaultBoundingBoxHeight)
+                else mempty)
+    ]
+render _ _ = mempty
+
+widthInUnits :: StartTerminator -> Double
+widthInUnits _ = 1.0
+
+heightInUnits :: StartTerminator -> Double
+heightInUnits _ = 1.0
